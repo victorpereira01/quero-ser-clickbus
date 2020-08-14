f9bf749a81f1b79ec930a7adfeeba767621cb77a
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-14 16:30:04 -0300

    Create a place and Get a specific place

--- backend-developer/solution/click-bus/src/main/java/com/victorpereira/clickbus/resources/PlaceResource.java
+++ backend-developer/solution/click-bus/src/main/java/com/victorpereira/clickbus/resources/PlaceResource.java
@@ -3,8 +3,13 @@ package com.victorpereira.clickbus.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorpereira.clickbus.models.Place;
@@ -20,5 +25,16 @@ public class PlaceResource {
 	@GetMapping
 	public List<Place> findAll() {
 		return service.findAll();
+	}
+	
+	@GetMapping(value="/{id}")
+	public Place findById(@PathVariable String id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping 
+	@ResponseStatus(HttpStatus.CREATED)
+	public Place insert(@RequestBody Place place) {
+		return service.insert(place);
 	}
 }
--- backend-developer/solution/click-bus/src/main/java/com/victorpereira/clickbus/services/PlaceService.java
+++ backend-developer/solution/click-bus/src/main/java/com/victorpereira/clickbus/services/PlaceService.java
@@ -1,6 +1,7 @@
 package com.victorpereira.clickbus.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,17 @@ public class PlaceService {
 
 	@Autowired
 	private PlaceRepository repo;
-	
+
 	public List<Place> findAll() {
 		return repo.findAll();
+	}
+
+	public Place findById(String id) {
+		Optional<Place> place = repo.findById(id);
+		return place.orElseThrow();
+	}
+
+	public Place insert(Place place) {
+		return repo.insert(place);
 	}
 }
